88319f017248ffe3c844a859f84bacbe73417324
Florian <florian.rief@outlook.de>
2025-08-25 23:06:49 +0200

    add install command

--- mvm.py
+++ mvm.py
@@ -1,12 +1,22 @@
 import argparse
 import os
 import re
+import shutil
 import sys
-from typing import Set
+import http.client
+import urllib.request
+import urllib.error
+from typing import Set, Final, Any
+from zipfile import ZipFile
 
 import import_config
 import mvn
 
+SERVERS: Final = {
+    "https://archive.apache.org/dist/maven/maven-4/": True,
+    "https://archive.apache.org/dist/maven/maven-3/": True,
+    "https://archive.apache.org/dist/maven/binaries/": False
+}
 config = import_config.get_config()
 MAVEN_PATH = config.MAVEN_PATH
 if not os.path.exists(MAVEN_PATH):
@@ -28,17 +38,61 @@ def scan_versions():
             versions.add(version)
     return versions
 
+def is_version_installed(version: str) -> bool:
+    installed_versions = scan_versions()
+    return version in installed_versions
+
 def list_command(_):
     versions = scan_versions()
     for version in versions:
         print(f"- {version}")
 
-parser = argparse.ArgumentParser('mvm')
+def install_command(args):
+    version = args.version
+    if is_version_installed(version):
+        print(f"version {version} is already installed")
+        return
+
+    error = None
+    for url, folders in SERVERS.items():
+        filename = f"apache-maven-{version}-bin.zip"
+        if folders:
+            url += f"{version}/binaries/"
+        url += filename
+
+        try:
+            response: http.client.HTTPResponse
+            out_file: Any
+            with urllib.request.urlopen(url) as response, open(filename, "wb") as out_file:
+                if response.status == http.HTTPStatus.OK:
+                    print(f"installing version {version}")
+                    shutil.copyfileobj(response, out_file)
+                    with ZipFile(filename) as zipfile:
+                        zipfile.extractall(config.MAVEN_PATH)
+                    out_file.close()
+                    os.remove(filename)
+                    print(f"successfully installed version {version}")
+                    return None
+        except Exception as ex:
+            error = ex
+
+    print(f"error installing version {version}")
+    print(error)
+
+parser = argparse.ArgumentParser("mvm")
 subparsers = parser.add_subparsers()
 
-parser_list = subparsers.add_parser('list', help='lists all installed versions under config.MAVEN_PATH')
+parser_list = subparsers.add_parser("list", help="lists all installed versions under config.MAVEN_PATH")
 parser_list.set_defaults(func=list_command)
 
-args = parser.parse_args()
-if hasattr(args, 'func'):
-    args.func(args)
+parser_install = subparsers.add_parser("install", help="installs the provided maven version to the config.MAVEN_PATH directory")
+parser_install.add_argument("version", help="version to install")
+parser_install.set_defaults(func=install_command)
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit(1)
+
+parser_args = parser.parse_args()
+if hasattr(parser_args, "func"):
+    parser_args.func(parser_args)
