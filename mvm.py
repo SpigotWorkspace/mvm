b7cefac27e6f0f6474d3de469c7415ba8083dce6
Florian <florian.rief@outlook.de>
2025-08-27 21:35:14 +0200

    add use command

--- mvm.py
+++ mvm.py
@@ -31,7 +31,7 @@ def scan_versions() -> dict[str, str]:
             is_executable = os.path.exists(mvn_path)
 
             if not is_executable: continue
-            match = re.search(r"apache-maven-(.+)", folder.name)
+            match: re.Match[str] = re.search(r"apache-maven-(.+)", folder.name)
             if match:
                 version = match.group(1)
             else:
@@ -47,13 +47,16 @@ def is_version_installed(version: str) -> bool:
 
 def list_command(_):
     versions = scan_versions()
-    for version in versions:
-        print(f"- {version}")
+    for version, folder_path in versions.items():
+        string = f"- {version}"
+        if folder_path == config.VERSION_TO_USE:
+            string += " (default)"
 
-def install_command(args):
-    version = args.version
+        print(string)
+
+def install_command(version):
     if is_version_installed(version):
-        print(f"version '{version}' is already installed")
+        print(f"Version '{version}' is already installed.")
         return None
 
     error = None
@@ -68,44 +71,58 @@ def install_command(args):
             out_file: Any
             with urllib.request.urlopen(url) as response, open(filename, "wb") as out_file:
                 if response.status == http.HTTPStatus.OK:
-                    print(f"installing version '{version}'")
+                    print(f"Installing version '{version}'.")
                     shutil.copyfileobj(response, out_file)
                     with ZipFile(filename) as zipfile:
                         zipfile.extractall(config.MAVEN_PATH)
                     out_file.close()
                     os.remove(filename)
-                    print(f"successfully installed version '{version}'")
+                    print(f"Successfully installed version '{version}'.")
                     return None
         except Exception as ex:
             error = ex
 
-    print(f"error installing version {version}")
+    print(f"Error installing version {version}.")
     print(error)
 
-def remove_command(args):
-    version = args.version
+def remove_command(version):
     installed_versions = scan_versions()
     if not version in installed_versions:
-        print(f"version '{version}' is not installed")
+        print(f"Version '{version}' is not installed.")
         return None
     folder_path = installed_versions.get(version)
     shutil.rmtree(folder_path)
 
-    print(f"successfully removed version '{version}'")
+    print(f"Successfully removed version '{version}'.")
+
+def use_command(version):
+    installed_versions = scan_versions()
+    if not version in installed_versions:
+        print(f"Version '{version}' is not installed.")
+        return None
+
+    folder_path = installed_versions.get(version)
+    config.VERSION_TO_USE = folder_path
+
+    print(f"Version {version} will now be used.")
 
 parser = argparse.ArgumentParser("mvm")
 subparsers = parser.add_subparsers()
 
-parser_list = subparsers.add_parser("list", help="lists all installed versions under config.MAVEN_PATH")
+parser_list = subparsers.add_parser("list", help="Lists all installed versions under config.MAVEN_PATH")
 parser_list.set_defaults(func=list_command)
 
-parser_install = subparsers.add_parser("install", help="installs the provided maven version to the config.MAVEN_PATH directory")
-parser_install.add_argument("version", help="version to install")
+parser_install = subparsers.add_parser("install", help="Installs the provided maven version to the config.MAVEN_PATH directory")
+parser_install.add_argument("version", help="Version to install")
 parser_install.set_defaults(func=install_command)
 
-parser_remove = subparsers.add_parser("remove", help="removes the provided maven version from the config.MAVEN_PATH directory")
-parser_remove.add_argument("version", help="version to remove")
+parser_remove = subparsers.add_parser("remove", help="Removes the provided maven version from the config.MAVEN_PATH directory")
+parser_remove.add_argument("version", help="Version to remove")
 parser_remove.set_defaults(func=remove_command)
+
+parser_use = subparsers.add_parser("use", help="Sets the maven version to use")
+parser_use.add_argument("version", help="Version to use")
+parser_use.set_defaults(func=use_command)
 
 if len(sys.argv) == 1:
     parser.print_help()
@@ -113,4 +130,7 @@ if len(sys.argv) == 1:
 
 parser_args = parser.parse_args()
 if hasattr(parser_args, "func"):
-    parser_args.func(parser_args)
+    version_arg = None
+    if hasattr(parser_args, "version"):
+        version_arg = parser_args.version
+    parser_args.func(version_arg)
--- mvn.py
+++ mvn.py
@@ -11,6 +11,12 @@ if __name__ == "__main__":
     import import_config
     config = import_config.get_config()
     MAVEN_HOME_PATH = os.path.join(config.VERSION_TO_USE, "bin", "mvn")
+    if not os.path.exists(MAVEN_HOME_PATH):
+        print(
+            f"Command cannot be executed because '{MAVEN_HOME_PATH}' does not exist.\n"
+            f"Please use 'mvm use' to select a valid version."
+        )
+        sys.exit(1)
 
     output = execute(MAVEN_HOME_PATH, sys.argv[1:])
     print(output)
